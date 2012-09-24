4bee5f31a107af2b0ac8e7d781d44cb2378f76f4
dbarsam <dbarsam@gmail.com>
2012-09-24 13:34:25 -0400

    Updated profile to Visual Studio 10's envvars.cmd

--- profile/profile.vs.cmd
+++ profile/profile.vs.cmd
@@ -8,13 +8,21 @@ REM ===================================================================
 REM ==================
 REM Custom Environment
 REM ==================
-if exist "%VS80COMNTOOLS%vsvars32.bat" (
-    call "%VS80COMNTOOLS%vsvars32.bat"
-)
 
+REM Invoke Visual Studio's Environment
+if exist "%VS100COMNTOOLS%vsvars32.bat" (
+    call "%VS100COMNTOOLS%vsvars32.bat"
+    goto ENVDONE
+) 
 if exist "%VS90COMNTOOLS%vsvars32.bat" (
     call "%VS90COMNTOOLS%vsvars32.bat"
+    goto ENVDONE
+) 
+if exist "%VS80COMNTOOLS%vsvars32.bat" (
+    call "%VS80COMNTOOLS%vsvars32.bat"
+    goto ENVDONE
 )
+:ENVDONE
 
 goto EXIT
 REM ==============
