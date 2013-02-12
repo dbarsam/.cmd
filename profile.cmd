77a275175ab37aa49f878f4af1031cd79aa3e64f
dbarsam <dbarsam@gmail.com>
2013-02-11 19:29:31 -0500

    Updated profile.cmd with more system doskey aliases

--- profile.cmd
+++ profile.cmd
@@ -13,10 +13,14 @@ REM ==============
 REM Custom Aliases
 REM ==============
 doskey ;alias=doskey /m
+doskey ;history=doskey /h
 doskey ;help=hh.exe ms-its:C:\WINDOWS\Help\ntcmds.chm::/ntcmds.htm
 doskey ;profile=edit %~f0
 doskey ;find=dir /s /b . ^| findstr /i /c:"$1" 
 doskey ;grep=findstr /s /n /i /m /c:"$1" $2
+doskey ;ex=explorer .
+doskey ;pd=pushd
+doskey ;po=popd
 
 REM ============
 REM External Configurations
