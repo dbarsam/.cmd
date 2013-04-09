90a6674fba2b4bbf18c50a3468cf0d0611ce8190
dbarsam <dbarsam@gmail.com>
2013-04-09 16:05:12 -0400

    Updated Get-Sysinternals command to use wget instead of robocopy.

--- scripts/Get-Sysinternals.cmd
+++ scripts/Get-Sysinternals.cmd
@@ -5,39 +5,72 @@ rem ===================================================================
 
 setlocal enableextensions enabledelayedexpansion
 
-
 rem ============
 rem Custom Help Message
 rem ============
-if "%1" == "help" goto HELP
+if %1 == "help" goto HELP
 
 rem ============
 rem Get the Installation folder
 rem ============
-if "%1" == "" (
+if %1 == "" (
     set TARGETPATH="C:\Program Files\Windows Sysinternals"
 ) else ( 
-    set TARGETPATH="%1"
+    set TARGETPATH=%1
+)
+
+if not exist !TARGETPATH! (
+    echo ERROR: [!TARGETPATH!]  doesn't exists
+    goto EXIT
+)
+
+rem ============
+rem Check For Wget in the path
+rem ============
+for %%f in (wget.exe) do (
+    if exist "%%~dp$PATH:f" (
+        set SCRIPTCOMMAND=wget.exe 
+        set SCRIPTCOMMAND=!SCRIPTCOMMAND! --user-agent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6" 
+        set SCRIPTCOMMAND=!SCRIPTCOMMAND! --referer="http://www.google.com" 
+        set SCRIPTCOMMAND=!SCRIPTCOMMAND! --timestamping 
+        set SCRIPTCOMMAND=!SCRIPTCOMMAND! --no-parent 
+        set SCRIPTCOMMAND=!SCRIPTCOMMAND! --recursive 
+        set SCRIPTCOMMAND=!SCRIPTCOMMAND! --cut-dirs=1 
+        set SCRIPTCOMMAND=!SCRIPTCOMMAND! --no-check-certificate 
+        set SCRIPTCOMMAND=!SCRIPTCOMMAND! --no-host-directories 
+        set SCRIPTCOMMAND=!SCRIPTCOMMAND! --no-directories 
+        set SCRIPTCOMMAND=!SCRIPTCOMMAND! --level=1 
+        set SCRIPTCOMMAND=!SCRIPTCOMMAND! --directory-prefix=!TARGETPATH!
+        set SCRIPTCOMMAND=!SCRIPTCOMMAND! --reject=txt,html,pdf,src 
+        set SCRIPTCOMMAND=!SCRIPTCOMMAND! http://live.sysinternals.com/Tools
+    ) else (
+        echo Cannot find %%f executable in the path
+    )
 )
 
 rem ============
 rem Check For RoboCopy in the path
 rem ============
-for %%f in (robocopy.exe) do (
-    if not exist "%%~dp$PATH:f" (
-        echo ERROR:  %%f Executable is not in path
-        goto EXIT
-    )
-)
+rem for %%f in (robocopy.exe) do (
+rem     if exist "%%~dp$PATH:f" (
+rem         set SCRIPTCOMMAND=robocopy.exe
+rem         set SCRIPTCOMMAND=%SCRIPTCOMMAND% \\live.sysinternals.com\tools
+rem         set SCRIPTCOMMAND=%SCRIPTCOMMAND% %TARGETPATH%
+rem         set SCRIPTCOMMAND=%SCRIPTCOMMAND% *.*
+rem         set SCRIPTCOMMAND=%SCRIPTCOMMAND% /COPY:DAT
+rem         set SCRIPTCOMMAND=%SCRIPTCOMMAND% /PURGE
+rem         set SCRIPTCOMMAND=%SCRIPTCOMMAND% /R:1
+rem         set SCRIPTCOMMAND=%SCRIPTCOMMAND% /W:3
+rem         set SCRIPTCOMMAND=%SCRIPTCOMMAND% /XF *.db *.url *.scr *.pdf About_This_Site.txt
+rem     ) else (
+rem         echo Cannot find %%f executable in the path
+rem     )
+rem )
 
 rem ============
-rem Copy the Files from live.sysinternals.com
+rem Execute the Command
 rem ============
-if exist %TARGETPATH% (
-    robocopy.exe \\live.sysinternals.com\tools %TARGETPATH% *.* /COPY:DAT /PURGE /R:1 /W:3 /XF *.db *.url *.scr *.pdf About_This_Site.txt
-) else (
-    echo ERROR: [%TARGETPATH%]  doesn't exists
-)
+if defined SCRIPTCOMMAND echo "!SCRIPTCOMMAND!" && !SCRIPTCOMMAND!
 goto EXIT
 
 rem ============
