58a805d475c726d132461089bc1be3845d6775e3
dbarsam <dbarsam@gmail.com>
2013-09-17 14:51:59 -0400

    Added a cmd profile extension for a 'python' environment

---
+++ profile/profile.python.cmd
@@ -0,0 +1,29 @@
+@echo off
+REM ===================================================================
+REM                  Dos Environment Set-up File
+REM -------------------------------------------------------------------
+REM                             Python
+REM ===================================================================
+
+REM ==============
+REM Custom Aliases
+REM ==============
+for %%p in ("%PROGRAMFILES%" "%PROGRAMFILES(x86)%") do (
+    for %%v in (26 27) do (
+        if exist "%%~p\python%%v\python.exe" (
+            doskey ;p%%v="%%~p\python%%v\python.exe"
+        )
+    )
+)
+
+goto EXIT
+REM ==============
+REM Error Handling
+REM ==============
+:ERROR
+
+REM ===================================================================
+REM End of Script
+REM ===================================================================
+:EXIT
+
