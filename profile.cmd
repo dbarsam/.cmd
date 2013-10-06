b4695e61afdb67d8728dc84380bf1ca6eabe6931
dbarsam <dbarsam@gmail.com>
2013-10-06 15:15:00 -0400

    Added desktop doskey alias

--- profile.cmd
+++ profile.cmd
@@ -19,6 +19,7 @@ doskey ;profile=edit %~f0
 doskey ;find=dir /s /b . ^| findstr /i /c:"$1" 
 doskey ;grep=findstr /s /n /i /m /c:"$1" $2
 doskey ;ex=explorer .
+doskey ;desktop=pushd %USERPROFILE%\desktop
 doskey ;pd=pushd $*
 doskey ;po=popd
 
