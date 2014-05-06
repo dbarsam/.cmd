340c1cbc3ea5560bb3589bcec617637f5843a33c
dbarsam <dbarsam@gmail.com>
2014-05-06 14:10:37 -0400

    Updated python.cmd to use x86 and x64 Program Files folder explicitly.

--- profile/profile.python.cmd
+++ profile/profile.python.cmd
@@ -9,13 +9,13 @@ REM Custom Aliases
 REM ==============
 doskey ;pyclean=del /s /f *.pyc *.pyo
 setlocal enableextensions enabledelayedexpansion
-for %%p in ("%PROGRAMFILES(x86)%" "%PROGRAMFILES%") do (
-    for /f "delims=" %%d in ('dir /b "%%~p\python*" ^| findstr /i /r "python[0-9][0-9]*$"') do (
+for %%p in ("%PROGRAMFILES(x86)%" "%PROGRAMW6432%") do (
+    for /f "delims=" %%d in ('dir /b "%%~p\python*" 2^>nul ^| findstr /i /r "python[0-9][0-9]*$"') do (
         if exist "%%p\%%d\python.exe" (
             set pver=%%d
             set pver=!pver:~6!
             if %%p=="%PROGRAMFILES(x86)%" doskey ;p!pver!32="%%~p\%%d\python.exe" $*
-            if %%p=="%PROGRAMFILES%"      doskey ;p!pver!="%%~p\%%d\python.exe" $*
+            if %%p=="%PROGRAMW6432%"      doskey ;p!pver!="%%~p\%%d\python.exe" $*
         )
     )
 )
